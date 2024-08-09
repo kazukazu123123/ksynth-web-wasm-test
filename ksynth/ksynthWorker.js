9a07ef137d86ae7abe25b5e88074ef775ad1af5d
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2024-08-09 20:47:15 +0900

    高負荷時に音がプチプチするけど、とりあえず滑らかになった！！！！(ついでにwasmも更新した(最適化ガッツリ掛けた))

--- ksynth/ksynthWorker.js
+++ ksynth/ksynthWorker.js
@@ -1,145 +1,175 @@
-importScripts('ksynth.js');
+importScripts("ksynth.js");
 
 let ksynthInstance = null;
 let sampleDataPtr = null;
 let filePathPtr = null;
-let bufferSize = 960;
 let sampleRate = 48000;
-let audioBuffers = [];
-let midiEventQueue = [];
 let isInitialized = false;
 let bufferInterval = null;
 
 function initializeKSynth(ksmpArrayBuffer) {
-    try {
-        const ksynthSampleBuffer = new Uint8Array(ksmpArrayBuffer);
-        const filePath = '/home/web_user/sample.ksmp';
+  if (ksynthInstance) return;
 
-        FS.writeFile(filePath, ksynthSampleBuffer);
+  try {
+    const ksynthSampleBuffer = new Uint8Array(ksmpArrayBuffer);
+    const filePath = "/home/web_user/sample.ksmp";
 
-        const sampleDataSize = ksynthSampleBuffer.byteLength;
-        sampleDataPtr = Module._malloc(sampleDataSize);
-        Module.HEAPU8.set(ksynthSampleBuffer, sampleDataPtr);
+    FS.writeFile(filePath, ksynthSampleBuffer);
 
-        const filePathLen = Module.lengthBytesUTF8(filePath) + 1;
-        filePathPtr = Module._malloc(filePathLen);
-        Module.stringToUTF8(filePath, filePathPtr, filePathLen);
+    const sampleDataSize = ksynthSampleBuffer.byteLength;
+    sampleDataPtr = Module._malloc(sampleDataSize);
+    Module.HEAPU8.set(ksynthSampleBuffer, sampleDataPtr);
 
-        const numChannel = 2;
-        const maxPolyphony = 500;
+    const filePathLen = Module.lengthBytesUTF8(filePath) + 1;
+    filePathPtr = Module._malloc(filePathLen);
+    Module.stringToUTF8(filePath, filePathPtr, filePathLen);
 
-        ksynthInstance = Module._ksynth_new(filePathPtr, sampleRate, numChannel, maxPolyphony, false);
-        if (ksynthInstance === null) {
-            console.error("Failed to create KSynth instance.");
-            return;
-        }
+    const numChannel = 2;
+    const maxPolyphony = 500;
 
-        Module._free(sampleDataPtr);
-        sampleDataPtr = undefined;
-        Module._free(filePathPtr);
-        filePathPtr = undefined;
-
-        isInitialized = true;
-
-        bufferInterval = setInterval(() => {
-            if (ksynthInstance) {
-                processMidiEvents();
-                generateAndStoreBuffer();
-            }
-        });
-
-        self.postMessage({ type: 'initialized' });
-
-    } catch (error) {
-        console.error("Initialization error:", error);
-        self.postMessage({ type: 'error', error });
-    } finally {
-        deleteFile('/home/web_user/sample.ksmp');
+    ksynthInstance = Module._ksynth_new(
+      filePathPtr,
+      sampleRate,
+      numChannel,
+      maxPolyphony,
+      false
+    );
+    if (ksynthInstance === null) {
+      console.error("Failed to create KSynth instance.");
+      return;
     }
+
+    Module._free(sampleDataPtr);
+    sampleDataPtr = undefined;
+    Module._free(filePathPtr);
+    filePathPtr = undefined;
+
+    isInitialized = true;
+
+    self.postMessage({ type: "initialized" });
+  } catch (error) {
+    console.error("Initialization error:", error);
+    self.postMessage({ type: "error", error });
+  } finally {
+    deleteFile("/home/web_user/sample.ksmp");
+  }
 }
 
-function generateAndStoreBuffer() {
-    if (ksynthInstance === null) return;
+function generateBuffer(bufferMultiply) {
+  if (ksynthInstance === null) return;
 
-    processMidiEvents();
+  const chunkSize = 960;
+  const totalSize = chunkSize * bufferMultiply;
 
-    if (audioBuffers.length < bufferSize * 4) {
-        const bufferPtr = Module._ksynth_generate_buffer(ksynthInstance, bufferSize);
+  const combinedBuffer = new Float32Array(totalSize);
+  let offset = 0;
 
-        if (bufferPtr === null) {
-            console.error("Failed to generate buffer. bufferPtr is null.");
-            return;
-        }
+  for (let i = 0; i < bufferMultiply; i++) {
+    const currentChunkSize = chunkSize;
+    const bufferPtr = Module._ksynth_generate_buffer(
+      ksynthInstance,
+      currentChunkSize
+    );
 
-        const buffer = new Float32Array(Module.HEAPF32.buffer, bufferPtr, bufferSize);
-
-        audioBuffers.push(...buffer);
-
-        Module._ksynth_buffer_free(bufferPtr);
+    if (bufferPtr === null) {
+      console.error("Failed to generate buffer. bufferPtr is null.");
+      return;
     }
-}
 
-function processMidiEvents() {
-    if (ksynthInstance === null || midiEventQueue.length === 0) return;
+    const chunk = new Float32Array(
+      Module.HEAPF32.buffer,
+      bufferPtr,
+      currentChunkSize
+    );
 
-    const events = midiEventQueue.splice(0, midiEventQueue.length);
+    combinedBuffer.set(chunk, offset);
+    offset += currentChunkSize;
 
-    events.forEach(event => {
-        if (event.type === 'noteOn') {
-            Module._ksynth_note_on(ksynthInstance, event.channel, event.note, event.velocity);
-        } else if (event.type === 'noteOff') {
-            Module._ksynth_note_off(ksynthInstance, event.channel, event.note);
-        }
-    });
+    Module._ksynth_buffer_free(bufferPtr);
+  }
+
+  self.postMessage({ type: "audioBuffer", buffer: [...combinedBuffer] });
 }
 
 function deleteFile(filePath) {
-    try {
-        FS.unlink(filePath);
-    } catch (error) {
-        console.error(`Failed to delete file ${filePath}:`, error);
-    }
+  try {
+    FS.unlink(filePath);
+  } catch (error) {
+    console.error(`Failed to delete file ${filePath}:`, error);
+  }
 }
 
 function destroyKSynth() {
-    if (isInitialized && ksynthInstance !== null) {
-        clearInterval(bufferInterval);
+  if (isInitialized && ksynthInstance !== null) {
+    clearInterval(bufferInterval);
 
-        // Free the KSynth instance
-        Module._ksynth_free(ksynthInstance);
-        ksynthInstance = null;
+    // Free the KSynth instance
+    Module._ksynth_free(ksynthInstance);
+    ksynthInstance = null;
 
-        // Clear any remaining buffers and MIDI events
-        audioBuffers.splice(0);
-        midiEventQueue.splice(0);
-        filePathPtr = undefined;
+    // Clear any remaining buffers and MIDI events
+    audioBuffers.splice(0);
+    filePathPtr = undefined;
 
-        isInitialized = false;
-    }
+    isInitialized = false;
+  }
 }
 
 self.onmessage = function (e) {
-    const message = e.data;
+  const message = e.data;
 
-    switch (message.type) {
-        case 'initialize':
-            initializeKSynth(message.sampleData);
-            break;
-        case 'midiEvent':
-            if (isInitialized) midiEventQueue.push(message.event);
-            break;
-        case 'getAudioData':
-            if (audioBuffers.length > bufferSize) {
-                const latestBuffer = audioBuffers.splice(0, bufferSize);
-                self.postMessage({ type: 'audioBuffer', buffer: latestBuffer });
-            } else {
-                self.postMessage({ type: 'audioBuffer', buffer: Array(bufferSize).fill(0) });
-            }
-            break;
-        case 'destroy':
-            destroyKSynth();
-            break;
-        default:
-            console.error('Unknown message type:', type);
-    }
+  switch (message.type) {
+    case "initialize":
+      initializeKSynth(message.sampleData);
+      break;
+    case "getInfo":
+      const polyphony = Module._ksynth_get_polyphony(ksynthInstance);
+      const maxPolyphony = Module._ksynth_get_max_polyphony(ksynthInstance);
+      const renderingTime = Module._ksynth_get_rendering_time(ksynthInstance);
+      self.postMessage({
+        type: "info",
+        polyphony,
+        maxPolyphony,
+        renderingTime,
+      });
+      break;
+    case "generateBuffer":
+      if (!message.bufferMultiply)
+        console.error("bufferMultiply parameter not specified.");
+      generateBuffer(message.bufferMultiply);
+      break;
+    case "setMaxPolyphony":
+      if (!isInitialized) return;
+      const newPolyphony = message.newPolyphony;
+      if (!newPolyphony)
+        return console.error("newPolyphony parameter not specified.");
+      Module._ksynth_set_max_polyphony(ksynthInstance, newPolyphony);
+      break;
+    case "note_off_all":
+      if (!isInitialized) return;
+      Module._ksynth_note_off_all(ksynthInstance);
+      break;
+    case "midiEvent":
+      if (!isInitialized) return;
+
+      switch (message.event.type) {
+        case "noteOn":
+          Module._ksynth_note_on(
+            ksynthInstance,
+            message.event.channel,
+            message.event.note,
+            message.event.velocity
+          );
+          break;
+        case "noteOff":
+          Module._ksynth_note_off(ksynthInstance, message.event.channel, message.event.note);
+          break;
+      }
+      break;
+    case "destroy":
+      destroyKSynth();
+      break;
+    default:
+      console.error("Unknown message type:", message.type);
+  }
 };
--- script.js
+++ script.js
@@ -1,68 +1,70 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const ksynthWorker = new Worker('ksynth/ksynthWorker.js');
+document.addEventListener('DOMContentLoaded', async () => {
+  const limiter = new Limiter();
+  const fileInput = document.getElementById('fileInput');
 
+  const polyphonyLabel = document.getElementById('polyphonyLabel');
+  const maxPolyphonyLabel = document.getElementById('maxPolyphonyLabel');
+  const renderingTimeLabel = document.getElementById('renderingTimeLabel');
+
+  const NoteONButton = document.getElementById('NoteONButton');
+  const NoteOFFButton = document.getElementById('NoteOFFButton');
+
+  const ksynthWorker = new Worker('ksynth/ksynthWorker.js');
   window.ksynthWorker = ksynthWorker;
 
-  const limiter = new Limiter();
   const player = new PCMF32Player(960, 2);
-
   player.onEnded(() => {
-    ksynthWorker.postMessage({ type: 'getAudioData' });
+    ksynthWorker.postMessage({ type: 'generateBuffer', bufferMultiply: 4 });
   });
 
-  ksynthWorker.onmessage = function (e) {
+  ksynthWorker.onmessage = (e) => {
     const { type } = e.data;
 
     switch (type) {
-        case 'initialized':
-            console.log('KSynth initialized');
-            ksynthWorker.postMessage({ type: 'getAudioData' });
-            break;
-        case 'audioBuffer':
-            const buffer = e.data.buffer;
-            if (buffer.length === 0) return;
-
-            for (let i = 0; i < buffer.length; i++) {
-              buffer[i] = limiter.limit(buffer[i]);
-            }
-
-            // Play buffer
-            player.play(buffer);
-            break;
-        case 'error':
-            console.error('Error from worker:', e.data.error);
-            break;
-        default:
-            console.error('Unknown message type:', type);
-      }
+      case 'initialized':
+        console.log('KSynth initialized');
+        setInterval(() => ksynthWorker.postMessage({ type: 'getInfo'}), 1000 / 5);
+        ksynthWorker.postMessage({ type: 'generateBuffer', bufferMultiply: 4 });
+        break;
+      case 'info':
+        polyphonyLabel.innerText = e.data.polyphony;
+        maxPolyphonyLabel.innerText = e.data.maxPolyphony;
+        renderingTimeLabel.innerText = e.data.renderingTime * 100.0;
+        break;
+      case 'audioBuffer':
+        const buffer = e.data.buffer;
+        const limitedBuffer = buffer.map(sample => limiter.limit(sample));
+        player.play(new Float32Array(limitedBuffer));
+        break;
+      case 'error':
+        console.error('Error from worker:', e.data.error);
+        break;
+      default:
+        console.error('Unknown message type:', type);
+    }
   };
 
   const handleFileSelect = async (event) => {
     const file = event.target.files[0];
     if (file) {
       const arrayBuffer = await file.arrayBuffer();
-
       ksynthWorker.postMessage({ type: 'initialize', sampleData: arrayBuffer });
     }
   };
 
-  const fileInput = document.getElementById('fileInput');
   if (fileInput) {
     fileInput.addEventListener('change', handleFileSelect);
   }
 
-  const NoteONButton = document.getElementById('NoteONButton');
-  const NoteOFFButton = document.getElementById('NoteOFFButton');
-
   if (NoteONButton) {
     NoteONButton.addEventListener('click', () => {
-      ksynthWorker.postMessage({ type: 'midiEvent', event: { type: 'noteOn', channel: 1, note: 60, velocity: 127 } });
+      ksynthWorker.postMessage({ type: 'midiEvent', event: { type: 'noteOn', channel: 0, note: 60, velocity: 127 } });
     });
   }
 
   if (NoteOFFButton) {
     NoteOFFButton.addEventListener('click', () => {
-      ksynthWorker.postMessage({ type: 'midiEvent', event: { type: 'noteOff', channel: 1, note: 60 } });
+      ksynthWorker.postMessage({ type: 'midiEvent', event: { type: 'noteOff', channel: 0, note: 60 } });
     });
   }
 });
